7036a62a61ff612c20344fb3d2539872d7be220d
Akriti Batra <akritibatra6@gmail.com>
2024-12-20 01:10:10 -0500

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,19 @@
+name = input("Name: ")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+place = input("Place: ")
+adj3 = input("Adjective: ")
+friend = input('Name: ')
+occupation = input('Job: ')
+ing = input("Verb ending in ing: ")
+noun1 = input("Noun: ")
+age = input('Number: ')
+
+
+madlib1 = f"Once upon a time there lived a princess named {name}. {name} was known for being the most {adj1} and {adj2} girl in the kingdom of \
+{place}. But little did eveyone else know, {name} had a sercet. One {adj3} morning, {name} snuck out of the palace to meet her friend {friend}. \
+{friend} was the town {occupation}, who was known for his trouble making ways. {friend} convinced {name} to start {ing} {noun1} and has been \
+addicted ever since. This addiction lead to her untimely death at age {age}"
+
+
+print(madlib1)
